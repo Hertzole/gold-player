05d6f5d03b8313eeba9d557435b5481f21ed986e
Hertzole <hampusringstrom@gmail.com>
2017-12-14 14:50:15 +0100

    PlayerBehaviour now has a shortcut to GoldPlayerController.

--- Assets/Hertzole/Gold Player/Scripts/Core/Editor/MovementSpeedsEditor.cs
+++ Assets/Hertzole/Gold Player/Scripts/Core/Editor/MovementSpeedsEditor.cs
@@ -1,39 +1,39 @@
-﻿using Hertzole.GoldPlayer.Core;
-using UnityEditor;
-using UnityEngine;
+//using Hertzole.GoldPlayer.Core;
+//using UnityEditor;
+//using UnityEngine;
 
-namespace Hertzole.GoldPlayer.Editor
-{
-    [CustomPropertyDrawer(typeof(MovementSpeeds))]
-    public class MovementSpeedsEditor : PropertyDrawer
-    {
-        public static readonly float gap = EditorGUIUtility.singleLineHeight + 2;
-        
-        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
-        {           
-            EditorGUI.BeginProperty(position, label, property);
-            position.height = EditorGUIUtility.singleLineHeight;
-            EditorGUI.Foldout(position, true, label); // TODO: Allow user to actually use the folout :)
-            
-            position.y += gap;
-            
-            var indent = EditorGUI.indentLevel;
-            EditorGUI.indentLevel = 1;
-            
-            EditorGUI.PropertyField(position, property.FindPropertyRelative("m_ForwardSpeed"));
-            position.y += gap;
-            EditorGUI.PropertyField(position, property.FindPropertyRelative("m_SidewaysSpeed"));
-            position.y += gap;
-            EditorGUI.PropertyField(position, property.FindPropertyRelative("m_BackwardsSpeed"));
-            position.y += gap;
-            EditorGUI.indentLevel = indent;
+//namespace Hertzole.GoldPlayer.Editor
+//{
+//    [CustomPropertyDrawer(typeof(MovementSpeeds))]
+//    public class MovementSpeedsEditor : PropertyDrawer
+//    {
+//        public static readonly float gap = EditorGUIUtility.singleLineHeight + 2;
 
-            EditorGUI.EndProperty();
-        }
+//        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+//        {
+//            EditorGUI.BeginProperty(position, label, property);
+//            position.height = EditorGUIUtility.singleLineHeight;
+//            EditorGUI.Foldout(position, true, label); // TODO: Allow user to actually use the folout :)
 
-        public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
-        {
-            return base.GetPropertyHeight(property, label) + gap * 3;
-        }
-    }
-}
+//            position.y += gap;
+
+//            var indent = EditorGUI.indentLevel;
+//            EditorGUI.indentLevel = 1;
+
+//            EditorGUI.PropertyField(position, property.FindPropertyRelative("m_ForwardSpeed"));
+//            position.y += gap;
+//            EditorGUI.PropertyField(position, property.FindPropertyRelative("m_SidewaysSpeed"));
+//            position.y += gap;
+//            EditorGUI.PropertyField(position, property.FindPropertyRelative("m_BackwardsSpeed"));
+//            position.y += gap;
+//            EditorGUI.indentLevel = indent;
+
+//            EditorGUI.EndProperty();
+//        }
+
+//        public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+//        {
+//            return base.GetPropertyHeight(property, label) + gap * 3;
+//        }
+//    }
+//}
--- Assets/Hertzole/Gold Player/Scripts/Core/Modules/PlayerBob.cs
+++ Assets/Hertzole/Gold Player/Scripts/Core/Modules/PlayerBob.cs
@@ -1,16 +1,51 @@
-﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerHeadBob : MonoBehaviour {
+namespace Hertzole.GoldPlayer.Core
+{
+    [System.Serializable]
+    public class PlayerBob : PlayerModule
+    {
+        [SerializeField]
+        private bool m_EnableBob = true;
+        public bool EnableBob { get { return m_EnableBob; } set { m_EnableBob = value; } }
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+        [Space]
+
+        [SerializeField]
+        private float m_BobFrequency = 1.5f;
+        public float BobFrequency { get { return m_BobFrequency; } set { m_BobFrequency = value; } }
+        [SerializeField]
+        private float m_BobHeight = 0.3f;
+        public float BobHeight { get { return m_BobHeight; } set { m_BobHeight = value; } }
+        [SerializeField]
+        private float m_SwayAngle = 0.5f;
+        public float SwayAngle { get { return m_SwayAngle; } set { m_SwayAngle = value; } }
+        [SerializeField]
+        private float m_SideMovement;
+        public float SideMovement { get { return m_SideMovement; } set { m_SideMovement = value; } }
+        [SerializeField]
+        private float m_HeightSpeed = 0.3f;
+        public float HeightSpeed { get { return m_HeightSpeed; } set { m_HeightSpeed = value; } }
+        [SerializeField]
+        private float m_StrideSpeed = 0.3f;
+        public float StrideSpeed { get { return m_StrideSpeed; } set { m_StrideSpeed = value; } }
+
+        [Space]
+
+        [SerializeField]
+        private float m_LandMove = 0.4f;
+        public float LandMove { get { return m_LandMove; } set { m_LandMove = value; } }
+        [SerializeField]
+        private float m_LandTilt = 20f;
+        public float LandTilt { get { return m_LandTilt; } set { m_LandTilt = value; } }
+        [SerializeField]
+        private float m_StrafeTilt = 3f;
+        public float StrafeTilt { get { return m_StrafeTilt; } set { m_StrafeTilt = value; } }
+
+        [Space]
+
+        [SerializeField]
+        private Transform m_BobTarget;
+        public Transform BobTarget { get { return m_BobTarget; } set { m_BobTarget = value; } }
+    }
 }
