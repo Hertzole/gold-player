a97a2a4422f21e6ca48f96298b90c882caa2676a
Hertzole <hampusringstrom@gmail.com>
2018-07-08 15:20:59 +0200

    Player interactions and assembly defintions
    [A] Player Interactions support.
    [A] Assembly definitions.
    [U] Updated ProBuilder package.

---
+++ Assets/Hertzole/Gold Player/Scripts/Interaction/Editor/GoldPlayerInteractableEditor.cs
@@ -0,0 +1,41 @@
+using UnityEditor;
+using UnityEngine;
+
+namespace Hertzole.GoldPlayer.Interaction.Editor
+{
+    [CustomEditor(typeof(GoldPlayerInteractable))]
+    internal class GoldPlayerInteractableEditor : UnityEditor.Editor
+    {
+        private SerializedProperty m_CanInteract;
+        private SerializedProperty m_IsHidden;
+        private SerializedProperty m_UseCustomMessage;
+        private SerializedProperty m_CustomMessage;
+        private SerializedProperty m_OnInteract;
+
+        private void OnEnable()
+        {
+            m_CanInteract = serializedObject.FindProperty("m_CanInteract");
+            m_IsHidden = serializedObject.FindProperty("m_IsHidden");
+            m_UseCustomMessage = serializedObject.FindProperty("m_UseCustomMessage");
+            m_CustomMessage = serializedObject.FindProperty("m_CustomMessage");
+            m_OnInteract = serializedObject.FindProperty("m_OnInteract");
+        }
+
+        public override void OnInspectorGUI()
+        {
+            serializedObject.Update();
+
+            EditorGUILayout.PropertyField(m_CanInteract);
+            EditorGUILayout.PropertyField(m_IsHidden);
+
+            EditorGUILayout.PropertyField(m_UseCustomMessage);
+            GUI.enabled = m_UseCustomMessage.boolValue;
+            EditorGUILayout.PropertyField(m_CustomMessage);
+            GUI.enabled = true;
+
+            EditorGUILayout.PropertyField(m_OnInteract);
+
+            serializedObject.ApplyModifiedProperties();
+        }
+    }
+}
