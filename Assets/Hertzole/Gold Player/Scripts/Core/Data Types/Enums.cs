b85ca6202b6b324dc821766dd82cddb030209de2
Nick Gravelyn <nick@gravelyn.com>
2019-02-03 22:42:55 -0800

    Adding run/crouch toggle options (#27)
    * Adding run/crouch toggle options
    
    * Improving run toggling for UntilNoInput mode
    
    Previously if you pressed run, then started walking, you wouldn't be running.
    This changes that so that you'll be running as long as you're holding the run
    button but then you can release it once you start moving and the running will
    toggle off when you let go of input. Definitely feels nicer.
    
    * Fixing a bug with UntilNoInput run toggle mode
    
    Players should be able to hold run before move and have the character start
    running. Once running the player should be able to stop running either by
    releasing the movement buttons OR by pressing run again. Previously there was
    a bug in that first part that made it impossible to stop running when pressing
    the run button again. This change detects whether the player has started
    running since their last no input frame and uses that to ensure the player can
    press the run button to stop running as they would expect.

--- Assets/Hertzole/Gold Player/Scripts/Core/Data Types/Enums.cs
+++ Assets/Hertzole/Gold Player/Scripts/Core/Data Types/Enums.cs
@@ -1,6 +1,60 @@
 namespace Hertzole.GoldPlayer.Core
 {
-    public enum RunAction { FasterThanRunSpeed = 0, FasterThanRunSpeedAndPressingRun = 1 }
-    public enum GroundCheckType { Sphere, Box }
-    public enum AudioTypes { Standard = 0, Custom = 2 }
+    public enum RunAction
+    {
+        FasterThanRunSpeed = 0,
+        FasterThanRunSpeedAndPressingRun = 1
+    }
+
+    public enum GroundCheckType
+    {
+        Sphere,
+        Box
+    }
+
+    public enum AudioTypes
+    {
+        Standard = 0,
+        Custom = 2
+    }
+
+    /// <summary>
+    /// Type of toggling mode for running.
+    /// </summary>
+    public enum RunToggleMode
+    {
+        /// <summary>
+        /// Toggle is off and run button must be held down to run.
+        /// </summary>
+        Off = 0,
+
+        /// <summary>
+        /// Running is permanently toggled. Player must press run
+        /// button again to toggle off.
+        /// </summary>
+        Permanent = 1,
+
+        /// <summary>
+        /// Running is toggled on until the player presses the run button
+        /// again OR there is no movement input from the player.
+        /// </summary>
+        UntilNoInput = 2,
+    }
+
+    /// <summary>
+    /// Type of toggling mode for crouching.
+    /// </summary>
+    public enum CrouchToggleMode
+    {
+        /// <summary>
+        /// Toggle is off and crouch button must be held down to crouch.
+        /// </summary>
+        Off = 0,
+
+        /// <summary>
+        /// Crouching is permanently toggled. Player must press crouch
+        /// button again to toggle off.
+        /// </summary>
+        Permanent = 1,
+    }
 }
