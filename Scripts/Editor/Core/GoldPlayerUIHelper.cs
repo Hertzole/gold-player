e17f168830dc11788587354f510f03e81b78380c
Hertzole <hampusringstrom@gmail.com>
2019-04-17 00:40:41 +0200

    GUI update!
    [A] Support for the new UIElements system when using Unity 2019.2 or newer on all custom editors. (Still WIP!)

---
+++ Scripts/Editor/Core/GoldPlayerUIHelper.cs
@@ -0,0 +1,29 @@
+﻿#if UNITY_2019_2_OR_NEWER
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace Hertzole.GoldPlayer.Editor
+{
+    public static class GoldPlayerUIHelper
+    {
+        public static VisualElement GetSpace(float space = 8)
+        {
+            return new VisualElement() { style = { height = space } };
+        }
+
+        public static Label GetHeaderLabel(string text)
+        {
+            return new Label(text)
+            {
+                style =
+                {
+                    unityFontStyleAndWeight = FontStyle.Bold,
+                    paddingTop = 9,
+                    paddingLeft = 0,
+                    paddingBottom = 0
+                }
+            };
+        }
+    }
+}
+#endif
