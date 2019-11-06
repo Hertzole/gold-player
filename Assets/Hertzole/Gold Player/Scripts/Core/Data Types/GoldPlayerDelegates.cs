bfd57113f2ab1f0e4d5ecebb0ebe44cdabdbc680
Hampus Ringström <hampusringstrom@gmail.com>
2019-11-06 01:17:18 +0100

    1.4 updates.
    Also removed everything that isn't needed.

--- Assets/Hertzole/Gold Player/Scripts/Core/Data Types/GoldPlayerDelegates.cs
+++
@@ -1,8 +0,0 @@
-namespace Hertzole.GoldPlayer.Core
-{
-    public class GoldPlayerDelegates
-    {
-        public delegate void PlayerEvent();
-        public delegate void JumpEvent(float height);
-    }
-}
