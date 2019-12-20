5d4066e06761b336911d3db3ac190076520e0134
Hampus Ringström <hampusringstrom@gmail.com>
2019-12-20 13:58:42 +0100

    Interaction improvements.
    [C] Interaction now looks for IGoldPlayerInteractable interface instead so you can create your own interactable objects easily through script.
    [F] NullReferenceException in GoldPlayerInteraction.

---
+++ Scripts/Interaction/IGoldPlayerInteractable.cs
@@ -0,0 +1,18 @@
+﻿namespace Hertzole.GoldPlayer.Interaction
+{
+    /// <summary>
+    /// Used to easily hook into the GoldPlayerInteraction component. It will target this interface.
+    /// </summary>
+    public interface IGoldPlayerInteractable
+    {
+        bool UseCustomMessage { get; }
+
+        string CustomMessage { get; }
+
+        bool CanInteract { get; }
+
+        bool IsHidden { get; }
+
+        void Interact();
+    }
+}
