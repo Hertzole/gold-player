020d046c84373a5f736861225f80cec40eb1e431
Hampus Ringström <hampusringstrom@gmail.com>
2020-01-02 01:25:06 +0100

    Minor improvements.

--- Scripts/Interaction/GoldPlayerInteractable.cs
+++ Scripts/Interaction/GoldPlayerInteractable.cs
@@ -4,7 +4,7 @@ using UnityEngine.Serialization;
 
 namespace Hertzole.GoldPlayer.Interaction
 {
-    [AddComponentMenu("Gold Player/Interaction/Player Interactable")]
+    [AddComponentMenu("Gold Player/Interaction/Gold Player Interactable")]
     [DisallowMultipleComponent]
     public class GoldPlayerInteractable : MonoBehaviour, IGoldPlayerInteractable
     {
