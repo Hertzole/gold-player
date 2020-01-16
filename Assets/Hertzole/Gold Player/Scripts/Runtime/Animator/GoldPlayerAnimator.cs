e01e8f8bbf219f38580d4ef69aee637146a8da0d
Hampus Ringström <hampusringstrom@gmail.com>
2020-01-17 00:09:12 +0100

    Added simple player animator.
    The player animator is not done. It's missing specific states for jumping and crouching. Right now there's only a running animation but all different animations will be provided for the test character.

---
+++ Assets/Hertzole/Gold Player/Scripts/Runtime/Animator/GoldPlayerAnimator.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+
+namespace Hertzole.GoldPlayer.Animator
+{
+    public class GoldPlayerAnimator : MonoBehaviour
+    {
+        [SerializeField]
+        private UnityEngine.Animator animator = null;
+        [SerializeField]
+        private string moveX = "MoveX";
+        [SerializeField]
+        private string moveZ = "MoveZ";
+
+        [SerializeField]
+        [HideInInspector]
+        private CharacterController controller = null;
+
+        private int moveXHash;
+        private int moveZHash;
+
+        private void Awake()
+        {
+            moveXHash = UnityEngine.Animator.StringToHash(moveX);
+            moveZHash = UnityEngine.Animator.StringToHash(moveZ);
+        }
+
+        // Update is called once per frame
+        private void Update()
+        {
+            Vector3 velocity = transform.InverseTransformDirection(controller.velocity);
+            velocity /= 6;
+
+            animator.SetFloat(moveXHash, velocity.x);
+            animator.SetFloat(moveZHash, velocity.z);
+        }
+
+#if UNITY_EDITOR
+
+        private void OnValidate()
+        {
+            GetStandardComponents();
+        }
+
+        private void Reset()
+        {
+            GetStandardComponents();
+        }
+
+        private void GetStandardComponents()
+        {
+            if (controller == null)
+            {
+                controller = GetComponent<CharacterController>();
+            }
+        }
+
+#endif
+    }
+}
