b27154bb663889cf6e17da72391844b150b98a40
Hertzole <hampusringstrom@gmail.com>
2022-08-13 15:29:19 +0200

    add: support for PlayerInput in input system comp

--- Assets/Hertzole/Gold Player/Scripts/Runtime/Core/GoldPlayerInputSystem.cs
+++ Assets/Hertzole/Gold Player/Scripts/Runtime/Core/GoldPlayerInputSystem.cs
@@ -27,6 +27,9 @@ namespace Hertzole.GoldPlayer
     public class GoldPlayerInputSystem : MonoBehaviour, IGoldInput
     {
 #if !OBSOLETE
+        [SerializeField] 
+        [Tooltip("The player input to get the actions from.")]
+        private PlayerInput playerInput = default;
         [SerializeField]
         [FormerlySerializedAs("input")]
         [Tooltip("The input asset to get all actions from.")]
@@ -49,9 +52,24 @@ namespace Hertzole.GoldPlayer
         [System.Obsolete("Use 'InputAsset' instead. This will be removed on build.", true)]
         public InputActionAsset Input { get { return InputAsset; } set { InputAsset = value; } }
 #endif
+	    /// <summary> The player input to get the actions from. </summary>
+	    public PlayerInput PlayerInput 
+		{
+		    get { return playerInput; }
+		    set
+		    {
+			    if (playerInput != value)
+			    {
+					playerInput = value;
+					UpdateActions();
+			    }
+		    } 
+	    }
         /// <summary> The input asset to get all actions from. </summary>
         public InputActionAsset InputAsset { get { return inputAsset; } set { inputAsset = value; } }
-        private Dictionary<int, InputAction> actionsDictionary;
+        private readonly Dictionary<int, InputAction> actionsDictionary = new Dictionary<int, InputAction>();
+        
+        protected InputActionAsset ActionsAsset { get { return playerInput != null ? playerInput.actions : inputAsset; } }
 #endif
 
         /// <summary> Has input been enabled? </summary>
@@ -65,8 +83,8 @@ namespace Hertzole.GoldPlayer
         /// <summary> All the available actions. </summary>
         public InputSystemItem[] Actions { get { return actions; } set { actions = value; } }
 #endif
-
-        private void Start()
+	    
+	    private void Start()
         {
 #if OBSOLETE
             Debug.LogError(gameObject.name + " has GoldPlayerInputSystem added. It does not work on the legacy input manager.");
@@ -94,7 +112,7 @@ namespace Hertzole.GoldPlayer
                         continue;
                     }
 #endif
-                    actions[i].action.action.Enable();
+                    ActionsAsset[actions[i].actionName].Enable();
                 }
                 enabledInput = true;
             }
@@ -120,7 +138,7 @@ namespace Hertzole.GoldPlayer
                         continue;
                     }
 #endif
-                    actions[i].action.action.Disable();
+                    ActionsAsset[actions[i].actionName].Disable();
                 }
                 enabledInput = false;
             }
@@ -253,15 +271,16 @@ namespace Hertzole.GoldPlayer
         // Add all actions to the actions dictionary.
         public void UpdateActions()
         {
-            if (actions == null)
+            if (actions == null || actions.Length == 0)
             {
+	            actionsDictionary.Clear();
                 return;
             }
 
-            actionsDictionary = new Dictionary<int, InputAction>();
+            actionsDictionary.Clear();
             for (int i = 0; i < actions.Length; i++)
             {
-                actionsDictionary.Add(GoldPlayerController.InputNameToHash(actions[i].actionName), actions[i].action);
+                actionsDictionary.Add(GoldPlayerController.InputNameToHash(actions[i].actionName), ActionsAsset[actions[i].actionName]);
             }
         }
 #endif // !OBSOLETE
@@ -363,7 +382,7 @@ namespace Hertzole.GoldPlayer
             {
                 return Vector2.zero;
             }
-
+            
             return actionsDictionary[action].ReadValue<Vector2>();
 #else
             return Vector2.zero;
@@ -382,7 +401,7 @@ namespace Hertzole.GoldPlayer
             }
 #endif
 
-            if (actionsDictionary == null)
+            if (actionsDictionary.Count != actions.Length)
             {
                 UpdateActions();
             }
